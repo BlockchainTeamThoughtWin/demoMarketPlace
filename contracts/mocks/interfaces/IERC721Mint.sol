dbe675a2d9a00b5fa49dabd40765d37b67e3a8ec
sanjaykaushik29 <thoughtwin.sanjay@gmail.com>
2022-10-27 12:47:16 +0530

    Auction branch code

---
+++ contracts/mocks/interfaces/IERC721Mint.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+interface IERC721Mint is IERC721{
+    function mint(
+        address to,
+        uint96 _royality,
+         string memory tokenURI
+    ) external returns (uint256);
+}
