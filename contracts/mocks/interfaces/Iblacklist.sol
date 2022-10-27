f8a6979b10af6c73076f60d97d9ea8d0b1abcd6d
sanjaykaushik29 <thoughtwin.sanjay@gmail.com>
2022-10-27 13:24:54 +0530

    add

--- contracts/mocks/interfaces/Iblacklist.sol
+++ contracts/mocks/interfaces/Iblacklist.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.4;
 
 interface IblackList {
-    function _isPermitted(address user) external view returns (bool);
+      function _isPermitted( address user) external view returns(bool) ;
 
-    function AddRemoveBlacklist(address node) external;
-}
+    function AddRemoveBlacklist(address node) external ;
+ }
